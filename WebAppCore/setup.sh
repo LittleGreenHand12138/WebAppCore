e2938ad52d7e9d9fe37b1f82d9f927a27b6414b0
钰驰 伍 <yuchi.Wu@outlook.com>
2020-01-21 15:46:06 +0800

    fabu

---
+++ WebAppCore/setup.sh
@@ -0,0 +1,13 @@
+﻿image_version=`date +%Y%m%d%H%M`;
+echo $image_version;
+# cd WebAppCore
+git pull --rebase origin master;
+docker stop webappcore;
+docker rm webappcore;
+docker build -t webappcore:$image_version .;
+docker images;
+docker run -p 8080:80 -d --name webappcore webappcore:$image_version;
+# -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
+docker logs webappcore;
+#删除build过程中产生的镜像    #docker image prune -a -f
+docker rmi $(docker images -f "dangling=true" -q)
